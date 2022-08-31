32508b4f998d82325e9914cc8c986d3f8501b8ac
IonovaNatali <ionovanv@gmail.ru>
2022-09-01 00:23:37 +0300

    Задание 06

--- Program.cs
+++ Program.cs
@@ -1,32 +1,10 @@
 ﻿Console.WriteLine("Введите целое число  ");
-    int number1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите целое число  ");
-    int number2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите целое число  ");
-    int number3 = Convert.ToInt32(Console.ReadLine());
-
-int max = number1;
-
-if (number2 > number1)
+int userNumber = Convert.ToInt32(Console.ReadLine());
+if (userNumber % 2 == 0)
 {
-    if (number2 > number3)
-    {
-        max = number2;
-        Console.WriteLine(number2 + " - это максимум ");
-    }
-    else
-    
-        max = number3;
-        Console.WriteLine(number3 + " - это максимум ");
-
+    Console.WriteLine("Да");
 }
 else
-{
-    if (number3 > number1)
     {
-        max = number3;
-        Console.WriteLine(number3 + " - это максимум ");
+        Console.WriteLine("Нет");
     }
-    else
-    Console.WriteLine(number1 + " - это максимум ");
-}
